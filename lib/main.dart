0bb149d79da1b4af52bce3a150f17fca8a86aeee
naman <namanramphale625@gmail.com>
2025-01-03 11:57:10 +0530

    todays codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'auto_commit.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
+class Movie {
+  final String title;
+  final String poster;
+
+  Movie({
+    required this.title,
+    required this.poster,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      title: json['Title'] ?? 'N/A',
+      poster: json['Poster'] ?? '',
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,39 +41,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _searchController = TextEditingController();
+  List<Movie> _movies = [];
+  bool _isLoading = false;
+
+  Future<void> fetchMovies(String query) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    const baseUrl = 'http://www.omdbapi.com/?apikey=d5ef06e6';
+    final url = '$baseUrl&t=$query';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      if (json['Response'] == 'True') {
+        setState(() {
+          _movies = [Movie.fromJson(json)];
+        });
+      } else {
+        setState(() {
+          _movies = [];
+        });
+      }
+    } else {
+      setState(() {
+        _movies = [];
+      });
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text("Jio Cinema",style: TextStyle(
-            color: Colors.white,
-          ),),
-          backgroundColor: Colors.purpleAccent,
-          leading: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.menu,
-                color: Colors.purple,
-              )),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            hintText: 'Search movies...',
+            hintStyle: TextStyle(color: Colors.white70),
+            border: InputBorder.none,
+          ),
+          onSubmitted: (query) {
+            if (query.isNotEmpty) {
+              fetchMovies(query);
+            }
+          },
         ),
-        body: Row(
-          children: [
-            SizedBox(
-              height: 50,
+        backgroundColor: Colors.purpleAccent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.purple,
+          ),
+        ),
+      ),
+      body: _isLoading
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : _movies.isEmpty
+          ? const Center(
+        child: Text(
+          'No movies found',
+          style: TextStyle(color: Colors.white),
+        ),
+      )
+          : GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // Number of posters per row
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 2 / 3, // Adjust for poster aspect ratio
+        ),
+        itemCount: _movies.length,
+        itemBuilder: (context, index) {
+          final movie = _movies[index];
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              movie.poster,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Center(
+                  child: Text(
+                    "Image not available",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                );
+              },
             ),
-            Text(
-              "letes Movies",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            Column(
-              children: [
-                Image(image: )
-              ],
-            )
-          ],
-        ));
+          );
+        },
+      ),
+    );
   }
 }
